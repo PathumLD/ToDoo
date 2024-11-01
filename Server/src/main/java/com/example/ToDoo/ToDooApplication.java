2e65611bcc08056aef523b6522526b96db539916
Pathum Lakshan Dissanayake <iamartseeker@gmail.com>
2024-11-01 12:44:13 +0530

    Initial Commit

---
+++ Server/src/main/java/com/example/ToDoo/ToDooApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToDoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDooApplication.class, args);
+	}
+
+}
